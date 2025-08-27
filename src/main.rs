43c942cbaddcf667597ec45088a263f73eaa99c6
Brad Dunbar <dunbarb2@gmail.com>
2025-08-27 12:28:22 -0400

    HTML

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{response::Html, routing::get, Router};
+
+static INDEX: &str = include_str!("index.html");
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello from fly.io!" }));
+    let app = Router::new().route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html(INDEX.trim())
 }
