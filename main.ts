702584e3d4e776a35ce596467a985cd6e1c69fa7
Brad Dunbar <dunbarb2@gmail.com>
2025-09-05 18:26:20 -0400

    boom

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+const BOOM = new URLPattern({ pathname: '/boom' })
+
 Deno.serve({ port: 4444 }, (request): Response => {
+
+  const match = BOOM.exec(request.url)
+
+  if (match) {
+    return new Response("boom")
+  }
+
   return new Response("Bill Frye!")
 })
