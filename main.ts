fbafeab804b92f44d11461cb02e1ad2585d2c81e
Brad Dunbar <dunbarb2@gmail.com>
2025-09-13 07:26:42 -0400

    404

--- main.ts
+++ main.ts
@@ -4,26 +4,35 @@ import { buildAssets, readAssets, watchAssets } from "./assets.ts"
 
 const BOOM = new URLPattern({ pathname: "/boom" })
 const STATIC = new URLPattern({ pathname: "/static/*" })
+const INDEX = new URLPattern({ pathname: "/" })
+
+const route = async (request: Request): Promise<Response> => {
+  if (STATIC.exec(request.url)) {
+    return await serveDir(request, {
+      fsRoot: "static",
+      headers: [
+        "cache-control: max-age=315360000",
+      ],
+      urlRoot: "static",
+    })
+  }
+
+  const match = BOOM.exec(request.url)
+
+  if (match) {
+    return new Response("boom!")
+  }
+
+  if (INDEX.exec(request.url)) {
+    return index(request)
+  }
+
+  return new Response("404", { status: 404 })
+}
 
 const serve = () => {
-  Deno.serve({ port: 4444 }, (request) => {
-    if (STATIC.exec(request.url)) {
-      return serveDir(request, {
-        fsRoot: "static",
-        headers: [
-          "cache-control: max-age=315360000",
-        ],
-        urlRoot: "static",
-      })
-    }
-
-    const match = BOOM.exec(request.url)
-
-    if (match) {
-      return new Response("boom!")
-    }
-
-    const response = index(request)
+  Deno.serve({ port: 4444 }, async (request) => {
+    const response = await route(request)
 
     if (!response.headers.get("cache-control")) {
       response.headers.set("cache-control", "private,must-revalidate,max-age=0")
