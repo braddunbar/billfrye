be70df10a8fa2f198865f44a5532b059cd717f39
Brad Dunbar <dunbarb2@gmail.com>
2025-09-16 10:16:50 -0400

    favicon

--- layout.tsx
+++ layout.tsx
@@ -6,7 +6,8 @@ export const layout = (view: VNode, name: string) => {
   return `<!doctype html>
 <html>
 <head>
-  <meta charset='utf-8'>
+  <meta charset="utf-8">
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'><text font-size='1' y='.9'>🎸</text/></svg>">
   <script type="module" src="${assetUrl(name)}"></script>
 </head>
 <body>
